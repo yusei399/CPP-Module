44ebe13feb93a36a6335a2e0fea65dfb0e3398a9
yusei399 <yusei2021@keio.jp>
2022-09-27 17:36:41 +0900

    ex01

---
+++ 00/ex01/Contact.hpp
@@ -0,0 +1,27 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <iostream>
+#include <string.h>
+
+class Contact
+{
+private:
+	int			index;
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+public:
+	Contact();
+	~Contact();
+	int					get_index(void)			const;
+	const std::string	&get_firstname(void)		const;
+	const std::string	&get_lastname(void)		const;
+	const std::string	&get_nickname(void)		const;
+	void				set_index(int index);
+	void				set_firstname(std::string first_name);
+	void				set_lastname(std::string last_name);
+	void				set_nickname(std::string nickname);
+};
+
+#endif
