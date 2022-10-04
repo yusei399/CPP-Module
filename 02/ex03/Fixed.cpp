8920b188825c21fab460077c7e936e4d23e34a3e
yusei399 <yusei2021@keio.jp>
2022-10-04 15:45:15 +0900

    ex03

---
+++ 02/ex03/Fixed.cpp
@@ -0,0 +1,16 @@
+class Fixed
+{
+private:
+	/* data */
+public:
+	Fixed(/* args */);
+	~Fixed();
+};
+
+Fixed::Fixed(/* args */)
+{
+}
+
+Fixed::~Fixed()
+{
+}
---
+++ 03/ex00/ClapTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+	private:
+		std::string		_name;
+		unsigned int	_hp;
+		unsigned int	_energy;
+		unsigned int	_damage;
+		ClapTrap(void);
+	public:
+		ClapTrap(const std::string &_name);
+		ClapTrap(const ClapTrap &copy);
+		~ClapTrap(void);
+		ClapTrap	&operator=(ClapTrap &rhs);
+		void		attack(std::string const &target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+};
+
+
+#endif
