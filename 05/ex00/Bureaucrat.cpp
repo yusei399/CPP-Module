ab4e3b7161ee16b6e46b76844552b38875ab4267
yusei399 <yusei2021@keio.jp>
2022-10-13 14:37:14 +0900

    mod

--- 05/ex00/Bureaucrat.cpp
+++ 05/ex00/Bureaucrat.cpp
@@ -1,3 +1,30 @@
 #include "Bureaucrat.hpp"
 
-const *Bureaucrat::Gra
+const char *Bureaucrat::GradeTooHighException:: what() const throw()
+{
+	return ("The grade (1)");
+}
+
+
+const char *Bureaucrat::GradeTooLowException:: what() const throw()
+{
+	return (" The grade low (150)");
+}
+
+Bureaucrat::Bureaucrat(const std::string &name, int grade) :_name(name)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat() {}
+
+Bureaucrat::~Bureaucrat() {}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy) : _name(copy._name), _grade(copy._grade) {}
+
+
+
+
