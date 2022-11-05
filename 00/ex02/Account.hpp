a89a897e9f2e9947e0f33d8331eb3c5668995507
yusei399 <yusei2021@keio.jp>
2022-11-05 14:22:04 +0900

    test

--- 00/ex02/Account.hpp
+++ 00/ex02/Account.hpp
@@ -1,41 +1,39 @@
-#pragma once
-#ifndef __ACCOUNT_H__
-#define __ACCOUNT_H__
+class Account {
 
-class Account
-{
 
 public:
-	typedef Account t;
 
-	static int getNbAccounts(void);
-	static int getTotalAmount(void);
-	static int getNbDeposits(void);
-	static int getNbWithdrawals(void);
-	static void displayAccountsInfos(void);
+	typedef Account		t;
 
-	Account(int initial_deposit);
-	~Account(void);
+	static int	getNbAccounts( void );
+	static int	getTotalAmount( void );
+	static int	getNbDeposits( void );
+	static int	getNbWithdrawals( void );
+	static void	displayAccountsInfos( void );
 
-	void makeDeposit(int deposit);
-	bool makeWithdrawal(int withdrawal);
-	int checkAmount(void) const;
-	void displayStatus(void) const;
+	Account( int initial_deposit );
+	~Account( void );
+
+	void	makeDeposit( int deposit );
+	bool	makeWithdrawal( int withdrawal );
+	int		checkAmount( void ) const;
+	void	displayStatus( void ) const;
+
 
 private:
-	static int _nbAccounts;
-	static int _totalAmount;
-	static int _totalNbDeposits;
-	static int _totalNbWithdrawals;
 
-	static void _displayTimestamp(void);
+	static int	_nbAccounts;
+	static int	_totalAmount;
+	static int	_totalNbDeposits;
+	static int	_totalNbWithdrawals;
 
-	int _accountIndex;
-	int _amount;
-	int _nbDeposits;
-	int _nbWithdrawals;
+	static void	_displayTimestamp( void );
 
-	Account(void);
+	int				_accountIndex;
+	int				_amount;
+	int				_nbDeposits;
+	int				_nbWithdrawals;
+
+	Account( void );
+
 };
-
-#endif
