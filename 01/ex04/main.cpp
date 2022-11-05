fa97205ed1db7026484acc201751ba1a65e5b6df
yusei399 <yusei2021@keio.jp>
2022-11-05 15:10:50 +0900

    makefile

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -1,75 +1,63 @@
-#include <string>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 
-
-int main(int argc, char **argv)
+std::string read_to_string(std::ifstream& file)
 {
-	std::ifstream ifs;
-	std::ofstream ofs;
-	std::string	filename;
-	std::string	s1;
-	std::string s2;
-	char		buffer;
-	bool		judgement;
-	std::string	tmpbuffer;
-	int			i;
+    std::ostringstream ss;
 
-	filename = argv[1];
-	s1 = argv[2];
-	s2 = argv[3];
-	if (argc != 4 || argv[1][0] == '\0' || argv[2][0] == '\0' || argv[3][0] == '\0')
-	{
-		std::cout << "Error" << std::endl;
-		return (1);
-	}
-	ifs.open(filename);
-	ifs.open(filename + ".replace");
-	if (ifs.fail() || ofs.fail())
-	{
-		std::cout << "Failed to open file" << std::endl;
-		return (1);
-	}
-	while(!ifs.eof())
-	{
-		buffer = ifs.get();
-		if (ifs.eof())
-			break;
-		if (buffer != s1[0])
-			ofs << buffer;
-		else
-		{
-			if (s1.length() == 1)
-			{
-				if (!ifs.eof())
-					ofs << s2;
-				else
-				{
-					judgement = true;
-					i = 1;
-					tmpbuffer = buffer;
-					while (!ifs.eof())
-					{
-						buffer = ifs.get();
-						if (!ifs.eof())
-							tmpbuffer += buffer;
-						if (buffer != s1[i] || ifs.eof())
-						{
-							judgement = false;
-							break ;
-						}
-						if (i == s1.length() - 1)
-							break ;
-						i++;
-					}
-				if (judgement)
-					ofs << s2;
-				else
-					ofs << tmpbuffer;
-				}
-			}
-		}
-	}
-	ifs.close();
-	ofs.close();
+    ss << file.rdbuf();
+    return std::string(ss.str());
+}
+
+void replace(std::string& str, const std::string& s1, const std::string& s2)
+{
+    size_t index = str.find(s1, 0);
+
+    while (index != std::string::npos) {
+        str.erase(index, s1.length());
+        str.insert(index, s2);
+        index = index + s2.length();
+        index = str.find(s1, index);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 4) {
+        std::cerr << "Usage: " << *argv << " <file> <s1> <s2>\n";
+        return 1;
+    }
+
+    std::string filename(argv[1]);
+    std::string s1(argv[2]);
+    std::string s2(argv[3]);
+
+    if (s1.empty() || s2.empty()) {
+        std::cerr << "Can't use empty strings\n";
+        return 1;
+    }
+
+    std::ifstream input(filename.c_str());
+
+    if (!input.is_open()) {
+        std::cerr << "Can't open file " << filename << '\n';
+        return 1;
+    }
+
+    std::string content = read_to_string(input);
+    input.close();
+    replace(content, s1, s2);
+
+    filename += ".replace";
+    std::ofstream output(filename.c_str());
+
+    if (!output.is_open()) {
+        std::cerr << "Can't create file " << filename << '\n';
+        return 1;
+    }
+
+    output << content;
+    output.close();
 }
