e7b03663fd13507bfe24dab42df3bfed7637de37
yusei399 <yusei2021@keio.jp>
2023-03-19 01:48:11 +0900

    finish

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -1,16 +1,18 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+#include <algorithm>
+
 template <typename T>
-typename T::iterator easyfind(T container, int n)
+typename T::iterator easyfind(T &container, int n)
 {
-    return std::find(container.begin(), container.end(), n) != container.end();
+    return std::find(container.begin(), container.end(), n);
 }
 
 template <typename T>
-void test_vetor(T container, int n)
+void test_vetor(T &container, int n)
 {
-    if (easyfind(container, n))
+    if (easyfind(container, n) != container.end())
         std::cout << n << " found\n";
     else
         std::cout << n << " not found\n";
@@ -18,9 +20,9 @@ void test_vetor(T container, int n)
 
 
 template <typename T>
-void test_list(T container, int n)
+void test_list(T &container, int n)
 {
-    if (easyfind(container, n))
+    if (easyfind(container, n) != container.end())
         std::cout << n << " found\n";
     else
         std::cout << n << " not found\n";
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -15,4 +15,23 @@ int main()
     v.push_back(4);
     v.push_back(5);
     test_vetor(v, 1);
+    test_vetor(v, 2);
+    test_vetor(v, 3);
+    test_vetor(v, 4);
+    test_vetor(v, 5);
+    test_vetor(v, 6);
+
+    std::cout << " -----------------LIST----------------- "<< std::endl;
+    std::list<int> l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_back(3);
+    l.push_back(4);
+    l.push_back(5);
+    test_list(l, 1);
+    test_list(l, 2);
+    test_list(l, 3);
+    test_list(l, 4);
+    test_list(l, 5);
+    test_list(l, 6);
 }
