6ea603ac9717a0dc7e54190c20e4463ffbb21c29
yusei399 <yusei2021@keio.jp>
2023-05-21 00:54:53 +0900

    atof

--- 09/ex00/BitcoinExchange.cpp
+++ 09/ex00/BitcoinExchange.cpp
@@ -58,6 +58,74 @@ int Check_leap_year(int year, int month)
 		return 31;
 }
 
+std::string trim(const std::string& string, const char* trimCharacterList = " \t\v\r\n")
+{
+
+	std::string result;
+	std::string::size_type left = string.find_first_not_of(trimCharacterList);
+	if (left != std::string::npos)
+	{
+		std::string::size_type right = string.find_last_not_of(trimCharacterList);
+		result = string.substr(left, right - left + 1);
+	}
+	return (result);
+}
+
+size_t xStrlen(char *str)
+{
+	if(!str)
+		return (0);
+	size_t i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+bool	validateDate(std::string s_date, int &i_year,int &i_month,int &i_day)
+{
+	char 	*c_p_year;
+	char 	*c_p_month;
+	char 	*c_p_day;
+	
+
+	// std::cout << "test1" << std::endl;
+	c_p_year = strtok(const_cast<char*>(s_date.c_str()), "-");
+	// std::cout << "test2" << std::endl;
+
+	if (c_p_year == NULL)
+		return (1);
+	// std::cout << "test3" << std::endl;
+	c_p_year = (char *)trim(c_p_year).c_str();
+	// std::cout << "test4" << std::endl;
+	if (xStrlen(c_p_year) != 4 || atoi(c_p_year) > MAX_YEAR || atoi(c_p_year) < MIN_YEAR )
+		return (1);
+	else
+		i_year = atoi(c_p_year);
+	// std::cout << "test5" << std::endl;
+	c_p_month = strtok(NULL, "-");
+	if (c_p_month == NULL)
+		return (1);
+	// std::cout << "test6" << std::endl;
+	c_p_month = (char *)trim(c_p_month).c_str();
+	if (xStrlen(c_p_month) != 2 || atoi(c_p_month) > MAX_MONTH || atoi(c_p_month) < MIN_MONTH)
+		return (1);
+	else
+		i_month = atoi(c_p_month);
+	c_p_day = strtok(NULL, "-");
+	// std::cout << "test7" << std::endl;
+	if (c_p_day == NULL)
+		return (1);
+	// std::cout << "test8" << std::endl;
+	c_p_day = (char *)trim(c_p_day).c_str();
+	if (xStrlen(c_p_day) != 2 || atoi(c_p_day) > Check_leap_year(i_year, i_month) || atoi(c_p_day) < MIN_DAY)
+		return (1);
+	else
+		i_day = atoi(c_p_day);
+	// std::cout << "test9" << std::endl;
+	return (1);
+}
+
+
 
 bool isNumberArray(char* value_char_array)
 {
@@ -78,13 +146,14 @@ bool isNumberArray(char* value_char_array)
 bool Parse_date(std::string &line,BitcoinExchange &be)
 {
 	std::string data;
-	// float value;
+	float value;
 	char *data_array;
 	char *value_array;
+	std::map<std::string,std::string>::iterator its;
 	// int year, month, day;
 	// std::string month_str, day_str;
 	(void)be;
-	std::cout << std::count(line.begin(), line.end(), '|') << std::endl;
+	std::cout << "pipe : " << std::count(line.begin(), line.end(), '|') << std::endl;
 
 	if (std::count(line.begin(), line.end(), '|') > 1)
 	{
@@ -92,9 +161,49 @@ bool Parse_date(std::string &line,BitcoinExchange &be)
 		return 1;
 	}
 	data_array = strtok(const_cast<char*>(line.c_str()), "|");
-	return true;
-
+	std::cout << "data_array :"<< data_array << std::endl;
+	if (!data_array)
+	{
+		std::cout << "Error: file's line is not 'date | value'." << std::endl;
+		return 1;
+	}
 	value_array = strtok(NULL, "|");
 	data = std::string(data_array);
-	std::cout << data << std::endl;
+	// if (validateDate(data, year, month, day))
+	// {
+	// 	std::cout << "Error: bad input " << std::endl;
+	// 	return 1;
+	// }
+
+	if (!value_array)
+	{
+		std::cout << "Error: file's line is not 'date | value'." << std::endl;
+		return 1;
+	}
+	else
+	{
+		if (isNumberArray(value_array))
+		{
+			std::cout << "Error:  input is not number " << std::endl;
+			return 1;
+		}
+		else
+		{
+			std::cout << "value_array :"<< value_array << std::endl;
+			value = atof(value_array);
+			std::cout << "atof value :"<< value << std::endl;
+		}
+		if (value == 0 || value > 1000 || value < 0)
+		{
+			std::cout << "Error: value is not a float or a positive integer between 0 and 1000. " << std::endl;
+			return 1;
+		}
+		
+	}
+	// std::cout << "data :"<< data << std::endl;
+	std::cout << "value : "<< value_array << std::endl;
+
+	its = be.getEntries().find(data);
+
+	return true;
 }
--- 09/ex00/main.cpp
+++ 09/ex00/main.cpp
@@ -12,40 +12,50 @@ int main(int argc, char **argv)
     std::vector<std::string> lines;
     BitcoinExchange be;
 
-    std::ifstream csv("data.csv");
-    if (!csv.is_open())
-        throw std::runtime_error("Error: file not found");
-
-    while (std::getline(csv, line))
-    {
-        std::string key, value;
-        std::stringstream ss(line);
-
-        std::getline(ss, key, ',');
-        std::getline(ss, value, ',');
-
-        be.addEntry(key, value);
-    }
-    
-    // csv.close();
-    for (const auto &i : be.getEntries())
-        std::cout << i.first << ": " << i.second << std::endl;
-    
-    if (argc != 2)
-    {
-        std::cout << "Usage: ./bitcoin \"expression\"" << std::endl;
-        return 1;
-    }
-	std::ifstream ifs(argv[1]);
-	if (!ifs.is_open())
-		std::cerr << "Error: file not found" << std::endl;
-	if (Linecheck(ifs))
+	try
 	{
-		while (std::getline(ifs, line, '\n'))
+		std::ifstream csv("data.csv");
+		if (!csv.is_open())
+			throw std::runtime_error("Error: file not found");
+
+		while (std::getline(csv, line))
 		{
-			Parse_date(line, be);
+			std::string key, value;
+			std::stringstream ss(line);
+
+			std::getline(ss, key, ',');
+			std::getline(ss, value, ',');
+
+			be.addEntry(key, value);
 		}
+		
+		// csv.close();
+		for (const auto &i : be.getEntries())
+			std::cout << i.first << ": " << i.second << std::endl;
+		
+		if (argc != 2)
+		{
+			std::cout << "Usage: ./bitcoin \"expression\"" << std::endl;
+			return 1;
+		}
+		std::ifstream ifs(argv[1]);
+		if (!ifs.is_open())
+			std::cerr << "Error: file not found" << std::endl;
+		if (Linecheck(ifs))
+		{
+			while (std::getline(ifs, line, '\n'))
+			{
+				Parse_date(line, be);
+			}
+		}
+		
 	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+
     return 0;
 }
 
